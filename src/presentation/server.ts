82babdc8725d8f6f155a2dafc59f1fdf9d2ed154
Leandro Arturi <lea.arturi@gmail.com>
2024-02-29 20:37:10 -0300

    Install compressed

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,6 +1,6 @@
 import express, { Router } from 'express'
 import path from 'path'
-
+import compression from 'compression'
 interface Options {
   port: number
   routes: Router
@@ -24,6 +24,7 @@ export class Server {
   async start() {
     this.app.use(express.json())
     this.app.use(express.urlencoded({ extended: true })) // x-www-form-encoded
+    this.app.use(compression())
 
     // Public Folder
     this.app.use(express.static(this.publicPath))
