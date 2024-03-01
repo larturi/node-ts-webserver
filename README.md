041b2c7e01ad1b37898774f0038128cd34439ddb
Leandro Arturi <lea.arturi@gmail.com>
2024-03-01 18:30:17 -0300

    Tests

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 1. Clonar el `.env.template` y crear el `.env`
 
 2. Ejecutar `docker-compose up -d` para levantar la BD Postgres
+
+## Para tests
+
+1. Clonar el `.env.template` y crear el `.env.test`
+
+Esta integrado a https://console.neon.tech
--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,6 +1,20 @@
-describe('Testing App.ts', () => {
-    test('should work', () => {
-      expect(true).toBe(true)
+import { envs } from '../src/config/envs'
+import { Server } from '../src/presentation/server'
+
+jest.mock('../src/presentation/server')
+
+describe('Should call server with arguments and start', () => {
+  test('should work', async () => {
+    await import('../src/app')
+
+    expect(Server).toHaveBeenCalledTimes(1)
+
+    expect(Server).toHaveBeenCalledWith({
+      port: envs.PORT,
+      public_path: envs.PUBLIC_PATH,
+      routes: expect.any(Function)
     })
-    
-});
+
+    expect(Server.prototype.start).toHaveBeenCalledTimes(1)
+  })
+})
